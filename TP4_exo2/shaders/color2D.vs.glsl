d3cf15fea30be40ddfee3a13be600173f07b655c
Adriiix <adrrr2000@gmail.com>
2023-03-09 20:41:18 +0100

    particuuuule

--- TP4_exo2/shaders/color2D.vs.glsl
+++ TP4_exo2/shaders/color2D.vs.glsl
@@ -3,7 +3,9 @@
 layout(location = 0) in vec2 aVertexPosition;
 layout(location = 1) in vec3 aVertexColor;
 
+//les variables qui vont sortir pour aller dans l'autre shader
 out vec3 vFragColor;
+out vec2 vVertexPosition;
 
 mat3 translate(float tx, float ty){
   return mat3(
@@ -32,7 +34,12 @@ mat3 rotate(float a){
 
 void main() {
   vFragColor = aVertexColor; 
-  //gl_Position = vec4((translate(1,1)*vec3(aVertexPosition,1.)).xy, 0., 1.); // translation de (1,0)
-  gl_Position = vec4((rotate(180)*vec3(aVertexPosition,1.)).xy, 0., 1.); // translation de (1,0)
+  vVertexPosition = aVertexPosition; // on sort la variable position
 
+  //gl_Position = vec4((translate(1,1)*vec3(aVertexPosition,1.)).xy, 0., 1.); // translation de (1,0)
+  //gl_Position = vec4((rotate(180)*vec3(aVertexPosition,1.)).xy, 0., 1.); 
+  //gl_Position = vec4((translate(0.5,0)*rotate(45)*scale(0.5,0.5)*vec3(aVertexPosition,1.)).xy, 0., 1.); 
+  //gl_Position = vec4((rotate(45)*translate(0.5,0)*scale(0.5,0.5)*vec3(aVertexPosition,1.)).xy, 0., 1.); 
+  
+  gl_Position = vec4((vec3(aVertexPosition,1.)).xy, 0., 1.); 
 };
--- TP4_exo2/shaders/grey2d.fs.glsl
+++ TP4_exo2/shaders/grey2d.fs.glsl
@@ -1,9 +1,14 @@
 #version 330 core
 
 in vec3 vFragColor;
+in vec2 vVertexPosition; // la variale de position qui entre 
 
 out vec4 fFragColor;
 
 void main() {
-  fFragColor = vec4((vFragColor.x+vFragColor.y+vFragColor.z)/3, (vFragColor.x+vFragColor.y+vFragColor.z)/3, (vFragColor.x+vFragColor.y+vFragColor.z)/3, 1.);
+  float alpha=2;
+  float beta=70;
+  float a = alpha * exp(-beta * pow(distance(vVertexPosition,vec2(0,0)),2));
+  // fFragColor = vec4((vFragColor.x+vFragColor.y+vFragColor.z)/3, (vFragColor.x+vFragColor.y+vFragColor.z)/3, (vFragColor.x+vFragColor.y+vFragColor.z)/3, 1.); // pour le gris
+  fFragColor = vec4(a*vFragColor.x, a*vFragColor.y, a*vFragColor.z, 1.);
 };
